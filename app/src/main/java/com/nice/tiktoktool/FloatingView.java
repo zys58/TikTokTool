71d2469d2c4a0520f72882404d2f228e0f583295
maoyulong <1303348494@qq.com>
2018-11-29 17:18:55 +0800

    1.设置悬浮窗可拖动

--- app/src/main/java/com/nice/tiktoktool/FloatingView.java
+++ app/src/main/java/com/nice/tiktoktool/FloatingView.java
@@ -1,22 +1,12 @@
 package com.nice.tiktoktool;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Build;
-import android.os.Looper;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.view.*;
 import android.widget.FrameLayout;
-
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.nice.config.Config;
 import com.nice.service.TikTokAccessibilityService;
 
@@ -27,9 +17,22 @@ public class FloatingView extends FrameLayout {
     private Context mContext;
     private View mView;
     private ImageView beginBtn;
-    private int mTouchStartX, mTouchStartY;//手指按下时坐标
     private WindowManager.LayoutParams mParams;
     private FloatingManager mWindowManager;
+
+
+    private final int statusHeight;
+    int sW;
+    private float mTouchStartX;
+    private float mTouchStartY;
+    private float x;
+    private float y;
+    private float mLastX;
+    private float mLastY;
+    private float mStartX;
+    private float mStartY;
+    private long mLastTime;
+    private long mCurrentTime;
 
     public FloatingView(Context context) {
         super(context);
@@ -38,49 +41,77 @@ public class FloatingView extends FrameLayout {
         mView = mLayoutInflater.inflate(R.layout.float_static, null);
         beginBtn = mView.findViewById(R.id.begin_btn);
         mWindowManager = FloatingManager.getInstance(mContext);
-        beginBtn.setOnClickListener(new OnClickListener() {
+        statusHeight = getStatusHeight(context);
+
+        beginBtn.setOnTouchListener(new View.OnTouchListener() {
+
             @Override
-            public void onClick(View view) {
-                if (Config.getInstance(getContext()).getActivated()) {
-                    if (Config.getInstance(getContext()).getStatus()) {
-                        Config.getInstance(getContext()).setStatus(false);
-                        beginBtn.setImageResource(R.mipmap.start);
-                        TikTokAccessibilityService.attentionLetter = false;
-                        Toast.makeText(getContext(), "停止执行...", Toast.LENGTH_SHORT).show();
-                        TikTokAccessibilityService.privateLetterList.clear();
-                        TikTokAccessibilityService.attentionCount = 0;
-                    } else {
-                        Config.getInstance(getContext()).setStatus(true);
-                        beginBtn.setImageResource(R.mipmap.stop);
-                        Toast.makeText(getContext(), "开始执行...", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(getContext(), "软件未激活！", Toast.LENGTH_LONG).show();
-                }
-
-
-            }
-        });
-
-        mView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
+            public boolean onTouch(View v, MotionEvent event) {
+                //获取相对屏幕的坐标，即以屏幕左上角为原点
+                x = event.getRawX();
+                y = event.getRawY();
                 switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        mTouchStartX = (int) event.getRawX();
-                        mTouchStartY = (int) event.getRawY();
+                    case MotionEvent.ACTION_DOWN:  //捕获手指触摸按下动作
+                        //获取相对View的坐标，即以此View左上角为原点
+                        mTouchStartX = event.getX();
+                        mTouchStartY = event.getY();
+                        mStartX = event.getRawX();
+                        mStartY = event.getRawY();
+                        mLastTime = System.currentTimeMillis();
                         break;
-                    case MotionEvent.ACTION_MOVE:
-                        mParams.x += (int) event.getRawX() - mTouchStartX;
-                        mParams.y += (int) event.getRawY() - mTouchStartY;//相对于屏幕左上角的位置
-                        mWindowManager.updateView(mView, mParams);
+
+
+                    case MotionEvent.ACTION_MOVE:  //捕获手指触摸移动动作
+                        updateViewPosition();
                         break;
-                    case MotionEvent.ACTION_UP:
+
+
+                    case MotionEvent.ACTION_UP:  //捕获手指触摸离开动作
+                        mLastX = event.getRawX();
+                        mLastY = event.getRawY();
+
+                        mCurrentTime = System.currentTimeMillis();
+                        if (mCurrentTime - mLastTime < 1500) {
+                            if (Math.abs(mStartX - mLastX) < 10.0 && Math.abs(mStartY - mLastY) < 10.0) {
+                                //处理点击的事件
+                                click();
+                            }
+                        }
                         break;
                 }
                 return true;
             }
         });
+    }
+
+    private void updateViewPosition() {
+        WindowManager wm = (WindowManager) getContext()
+                .getSystemService(Context.WINDOW_SERVICE);
+        int width = wm.getDefaultDisplay().getWidth();
+        int height = wm.getDefaultDisplay().getHeight();
+        //更新浮动窗口位置参数
+        mParams.x = width - (int) x - (int) mTouchStartX;
+        mParams.y = (int) y - (height / 2);
+        mWindowManager.updateView(mView, mParams); //刷新显示
+    }
+
+    public void click() {
+        if (Config.getInstance(getContext()).getActivated()) {
+            if (Config.getInstance(getContext()).getStatus()) {
+                Config.getInstance(getContext()).setStatus(false);
+                beginBtn.setImageResource(R.mipmap.start);
+                TikTokAccessibilityService.attentionLetter = false;
+                Toast.makeText(getContext(), "停止执行...", Toast.LENGTH_SHORT).show();
+                TikTokAccessibilityService.privateLetterList.clear();
+                TikTokAccessibilityService.attentionCount = 0;
+            } else {
+                Config.getInstance(getContext()).setStatus(true);
+                beginBtn.setImageResource(R.mipmap.stop);
+                Toast.makeText(getContext(), "开始执行...", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "软件未激活！", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void show() {
@@ -105,6 +136,28 @@ public class FloatingView extends FrameLayout {
 
     public void hide() {
         mWindowManager.removeView(mView);
+    }
+
+    /**
+     * 获得状态栏的高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusHeight(Context context) {
+
+
+        int statusHeight = -1;
+        try {
+            Class clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
     }
 
 }
